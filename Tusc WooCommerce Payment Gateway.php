49598c3ba6de6cdb61abb3561ae6f8c477b65174
Kyle Sponable <kylesponable@gmail.com>
2020-08-31 16:09:35 -0400

    added draft function code

--- Tusc WooCommerce Payment Gateway.php
+++ Tusc WooCommerce Payment Gateway.php
@@ -118,3 +118,26 @@ function add_to_woo_tusc_payment_gateway( $gateways ) {
     $gateways[] = 'WC_Tusc_pay_Gateway';
     return $gateways;
 }
+
+// Method: POST, PUT, GET etc
+// Data: array("param" => "value") ==> index.php?param=value
+
+function CallAPI($method, $url, $data = false)
+{
+    $curl = curl_init();
+
+    switch ($method) {
+        case "POST":
+            curl_setopt($curl, CURLOPT_POST, 1);
+
+            if ($data)
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            break;
+        case "PUT":
+            curl_setopt($curl, CURLOPT_PUT, 1);
+            break;
+        default:
+            if ($data)
+                $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+}
